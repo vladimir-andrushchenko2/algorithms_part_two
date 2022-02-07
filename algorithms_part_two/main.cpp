2f2b42900f359ab64a85352a655607e7993b7126
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-02-07 13:13:40 +0300

    Initial Commit

---
+++ algorithms_part_two/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithms_part_two
+//
+//  Created by Vladimir Andrushchenko on 07.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
