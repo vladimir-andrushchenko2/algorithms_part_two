1a54b3071d47e883a3ab3eb50b72add9ff8d5a04
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-02-13 18:31:05 +0300

    implemented max heapify

--- Sprint5TaskA/main.cpp
+++ Sprint5TaskA/main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <cassert>
 #include <sstream>
+#include <iterator>
+#include <optional>
 
 const std::string test_input = R"d(5
 alla 4 100
@@ -18,12 +20,55 @@ int GetParent(int child_index) {
     return child_index / 2;
 }
 
-int GetLeftChild(int parent_index) {
+int IndexOfLeftChild(int parent_index) {
     return parent_index * 2;
 }
 
-int GetRightChild(int parent_index) {
+int IndexOfRightChild(int parent_index) {
     return parent_index * 2 + 1;
+}
+
+template<typename It>
+bool IsInsideHeap(It begin, It end, int index) {
+    return index < std::distance(begin, end);
+}
+
+template<typename It>
+int IndexOfMax(It begin, int index_left, int index_right) {
+    return *(begin + index_left) < *(begin + index_right) ? index_right : index_left;
+}
+
+template<typename It>
+void MaxHeapify(It begin, It end, const int index) {
+    // if no children
+    if (!IsInsideHeap(begin, end, IndexOfLeftChild(index))) {
+        return;
+    }
+    
+    // initialize with left child because we don't know if right child exists
+    int index_of_larger_child = IndexOfLeftChild(index);
+    
+    // if both children exist
+    if (IsInsideHeap(begin, end, IndexOfRightChild(index))) {
+        index_of_larger_child = IndexOfMax(begin, IndexOfLeftChild(index), IndexOfRightChild(index));
+    }
+    
+    if (IndexOfMax(begin, index, index_of_larger_child) != index) {
+        std::swap(*(begin + index), *(begin + index_of_larger_child));
+        MaxHeapify(begin, end, index_of_larger_child);
+    }
+}
+
+void Test() {
+    {
+        std::vector<int> test_heap = {-1, 1, 2, 3};
+        MaxHeapify(test_heap.begin(), test_heap.end(), 1);
+        
+        std::vector<int> desired_output = {-1, 3, 2, 1};
+        
+        assert(test_heap == desired_output);
+        std::cout << "Test MaxHeapify OK\n";
+    }
 }
 
 }
@@ -64,7 +109,9 @@ void RunProgram(std::istream& input) {
 }
 
 int main(int argc, const char * argv[]) {
-    std::stringstream test_input_stream{test_input};
-    RunProgram(test_input_stream);
+//    std::stringstream test_input_stream{test_input};
+//    RunProgram(test_input_stream);
+    
+    Heap::Test();
     return 0;
 }
