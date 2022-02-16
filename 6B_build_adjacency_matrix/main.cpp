4b2fdb2094f058f1e82954e67dd4f50f883783b6
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-02-16 22:36:58 +0300

    OK

--- 6B_build_adjacency_matrix/main.cpp
+++ 6B_build_adjacency_matrix/main.cpp
@@ -5,11 +5,22 @@ int main(int argc, const char * argv[]) {
     int vertexes_count, edges_count;
     std::cin >> vertexes_count >> edges_count;
     
-    std::vector<int> row(vertexes_count);
-    std::vector<std::vector<int>> matrix(row.size(), row);
+    std::vector<std::vector<int>> matrix(vertexes_count, std::vector<int>(vertexes_count));
+    
+    int from, to;
     
     for (int i = 0; i < edges_count; ++i) {
+        std::cin >> from >> to;
         
+        matrix[from - 1][to - 1] = 1;
+    }
+    
+    for (auto& row : matrix) {
+        for (auto& value : row) {
+            std::cout << value << ' ';
+        }
+        
+        std::cout << '\n';
     }
     
     return 0;
