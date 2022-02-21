be7ff369230c0f4af0406d20bb9b6a2d18bbd4cb
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-02-21 12:40:47 +0300

    initial commit

---
+++ 6J_topological_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  6J_topological_sort
+//
+//  Created by Vladimir Andrushchenko on 21.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
