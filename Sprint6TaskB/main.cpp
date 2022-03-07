6bffec9f1e13a5943731222f04c9e1bc66da2ca3
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-07 13:40:07 +0300

    can dfs through cities

--- Sprint6TaskB/main.cpp
+++ Sprint6TaskB/main.cpp
@@ -5,6 +5,7 @@
 #include <unordered_set>
 #include <set>
 #include <sstream>
+#include <deque>
 
 // Changed railroad types from R and B to Red and Blue to distinguish better
 enum class RailroadType {
@@ -19,7 +20,11 @@ AdjacencyList MakeAdjacencyList(int vertex_count) {
     return AdjacencyList(vertex_count + 1);
 }
 
-struct Graph {
+struct City {
+    std::deque<VertexId> roads_to_cities;
+};
+
+class Graph {
 public:
     Graph() = delete;
     
@@ -34,6 +39,33 @@ public:
         });
     }
     
+    void PrintDFS(std::ostream& output_stream) const {
+        auto is_visited = GetVisitedStatus();
+        
+        for (int i = 1; i < adjacency_list_.size(); ++i) {
+            if (!is_visited[i]) {
+                DFS(i, is_visited, [&output_stream](VertexId vertex){
+                    output_stream << vertex << ' ';
+                });
+            }
+        }
+    }
+    
+    void DFS(std::vector<City>& cities) const {
+        for (VertexId starting_city_id = 1; starting_city_id < adjacency_list_.size(); ++starting_city_id) {
+            auto is_visited = GetVisitedStatus();
+            
+            std::deque<VertexId> visited_vertexes;
+            
+            DFS(starting_city_id, is_visited, [&cities, starting_city_id](VertexId vertex){
+                cities[starting_city_id].roads_to_cities.push_back(vertex);
+            });
+        }
+    }
+    
+    int AdjacencyListSize() const {
+        return static_cast<int>(adjacency_list_.size());
+    }
     
 private:
     std::vector<bool> GetVisitedStatus() const {
@@ -63,7 +95,7 @@ std::pair<Graph, Graph> ReadDirectedGraphs(std::istream& input) {
     input >> vertexes_count >> std::ws;
     
     AdjacencyList adjacency_list_red(vertexes_count + 1);
-
+    
     AdjacencyList adjacency_list_blue(vertexes_count + 1);
     
     std::string connections_to_other_cities;
@@ -73,10 +105,10 @@ std::pair<Graph, Graph> ReadDirectedGraphs(std::istream& input) {
         
         for (int i = 0; i < connections_to_other_cities.size(); ++i) {
             char road_type = connections_to_other_cities[i];
-
+            
             // + 1 because first cities first road leads to second city
             VertexId to_vertex_id = from_vertex_id + i + 1;
-
+            
             if (road_type == 'R') {
                 adjacency_list_red[from_vertex_id].insert(to_vertex_id);
             } else if (road_type == 'B') {
@@ -97,12 +129,48 @@ BR
 R
 )d";
 
+void PrintCitiesConnections(const std::vector<City>& cities) {
+    for (VertexId city_id = 1; city_id < cities.size(); ++ city_id) {
+        std::cout << "city_id: " << city_id << " has roads leading to ";
+        
+        for (VertexId connected_city : cities[city_id].roads_to_cities) {
+            std::cout << connected_city << ' ';
+        }
+        
+        std::cout << '\n';
+    }
+}
+
 int main(int argc, const char * argv[]) {
     std::stringstream test_stream{test_string};
     
     auto [red_graph, blue_graph] = ReadDirectedGraphs(test_stream);
     
-    blue_graph.PrintDFS(std::cout, 2);
+    std::vector<City> cities_on_red_network(red_graph.AdjacencyListSize());
+    
+    red_graph.DFS(cities_on_red_network);
+    
+    PrintCitiesConnections(cities_on_red_network);
+    
+    std::cout << '\n';
+    
+    std::vector<City> cities_on_blue_network(blue_graph.AdjacencyListSize());
+    
+    blue_graph.DFS(cities_on_blue_network);
+    
+    PrintCitiesConnections(cities_on_blue_network);
+    
+//    for (VertexId city_id = 1; city_id < cities_on_red_network.size(); ++ city_id) {
+//        std::cout << "city_id: " << city_id << " has roads leading to ";
+//
+//        for (VertexId connected_city : cities_on_red_network[city_id].roads_to_cities) {
+//            std::cout << connected_city << ' ';
+//        }
+//
+//        std::cout << '\n';
+//    }
+    
+    
     
     return 0;
 }
