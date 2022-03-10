6af2e0fd6f121e1b845be7117cdf7b12ee1359f1
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-10 23:13:06 +0300

    new general direction haven't tested

--- Sprint6TaskB/main.cpp
+++ Sprint6TaskB/main.cpp
@@ -8,17 +8,11 @@
 #include <deque>
 #include <cassert>
 #include <stdexcept>
-#include <deque>
+#include <stack>
 
 using VertexId = int;
 
-static constexpr VertexId kFirstId = 1;
-
-using AdjacencyList = std::vector<std::set<VertexId>>;
-
-AdjacencyList MakeAdjacencyList(int vertex_count) {
-    return AdjacencyList(kFirstId + vertex_count);
-}
+using Matrix = std::vector<std::string>;
 
 struct City {
     std::unordered_map<VertexId, char> from;
@@ -28,99 +22,60 @@ class Graph {
 public:
     Graph() = delete;
     
-    Graph(AdjacencyList adjacency_list) :
-    adjacency_list_(std::move(adjacency_list)) {}
+    Graph(Matrix matrix) :
+    matrix_(std::move(matrix)), cities_(matrix_.size()) {}
     
-    template <typename Predicate>
-    void DFS(VertexId starting_vertex, Predicate predicate) const {
-        auto is_visited = GetVisitedStatus();
+    void DFS(VertexId current_id, std::pair<std::stack<VertexId>, char> previous) {
+        for (int i = 0; i < matrix_[current_id].size(); ++i) {
+            VertexId to_id = current_id + i + 1;
+            
+            if (matrix_[current_id][to_id] != previous.second) {
+                // assign new type
+                previous.second = matrix_[current_id][to_id];
+                
+                // clear stack
+                std::stack<VertexId> empty_temp;
+                std::swap(previous.first, empty_temp);
+            } else {
+                previous.first.push(current_id);
+            }
+            
+            DFS(to_id, previous);
+        }
         
-        std::deque<VertexId> previous;
-        
-        DFS(starting_vertex, is_visited, previous, predicate);
-    }
-    
-    int AdjacencyListSize() const {
-        return static_cast<int>(adjacency_list_.size());
-    }
-    
-private:
-    std::vector<bool> GetVisitedStatus() const {
-        return std::vector(adjacency_list_.size(), false);
-    }
-    
-    template<typename Predicate>
-    void DFS(VertexId start_id, std::vector<bool>& is_visited, std::deque<VertexId>& previous, Predicate predicate) const {
-        predicate(start_id, previous);
-        
-        previous.push_back(start_id);
-        
-        is_visited[start_id] = true;
-        
-        for (VertexId adjacent_vertex : adjacency_list_[start_id]) {
-            if (!is_visited[adjacent_vertex]) {
-                DFS(adjacent_vertex, is_visited, previous, predicate);
+        while (!previous.first.empty()) {
+            VertexId previous_city = previous.first.top();
+            previous.first.pop();
+            
+            if (cities_[current_id].from.count(previous_city) > 0 && cities_[current_id].from[previous_city] != previous.second) {
+                throw std::logic_error("duplicate route");
+            } else {
+                cities_[current_id].from[previous_city] = previous.second;
             }
         }
-        
-        previous.pop_back();
     }
     
 private:
-    AdjacencyList adjacency_list_;
+    Matrix matrix_;
+    std::vector<City> cities_;
 };
 
-void ConnectCitiesAccordingToGraphWithRoadsOfType(std::vector<City>& cities, const Graph& graph, char type) {
-    std::vector<bool> is_visited(graph.AdjacencyListSize(), false);
-    
-    for (VertexId starting_city_id = kFirstId; starting_city_id < cities.size(); ++starting_city_id) {
-        if (!is_visited[starting_city_id]) {
-            graph.DFS(starting_city_id, [&](VertexId current_city, const std::deque<VertexId>& previous){
-                is_visited[current_city] = true;
-                    
-                for (VertexId previously_visited : previous) {
-                    if (cities[current_city].from.count(previously_visited) > 0 && cities[current_city].from[previously_visited] != type) {
-                        throw std::logic_error("duplicate connection of different road types");
-                    }
-                    
-                    cities[current_city].from[previously_visited] = type;
-                }
-            });
-        }
-    }
-}
-
-std::pair<Graph, Graph> ReadDirectedGraphs(std::istream& input) {
+Graph ReadDirectedGraphs(std::istream& input) {
     int vertexes_count;
     
     input >> vertexes_count >> std::ws;
     
-    auto adjacency_list_red = MakeAdjacencyList(vertexes_count);
+    Matrix matrix;
     
-    auto adjacency_list_blue = MakeAdjacencyList(vertexes_count);
+    std::string roads_from_city;
     
-    std::string connections_to_other_cities;
-    
-    for (VertexId from_vertex_id = 1; from_vertex_id < vertexes_count; ++from_vertex_id) {
-        std::getline(input, connections_to_other_cities);
+    for (int i = 0; i < vertexes_count; ++i) {
+        std::getline(input, roads_from_city);
         
-        for (int i = 0; i < connections_to_other_cities.size(); ++i) {
-            char road_type = connections_to_other_cities[i];
-            
-            // + 1 because first cities first road leads to second city
-            VertexId to_vertex_id = from_vertex_id + i + 1;
-            
-            if (road_type == 'R') {
-                adjacency_list_red[from_vertex_id].insert(to_vertex_id);
-            } else if (road_type == 'B') {
-                adjacency_list_blue[from_vertex_id].insert(to_vertex_id);
-            } else {
-                assert(false && "Roadtype must be R for Red or B for Blue");
-            }
-        }
+        matrix.push_back(std::move(roads_from_city));
     }
     
-    return {adjacency_list_red, adjacency_list_blue};
+    return {std::move(matrix)};
 }
 
 
@@ -128,20 +83,6 @@ std::pair<Graph, Graph> ReadDirectedGraphs(std::istream& input) {
 
 
 int main(int argc, const char * argv[]) {
-    auto [red_graph, blue_graph] = ReadDirectedGraphs(std::cin);
-    
-    std::vector<City> cities(red_graph.AdjacencyListSize());
-    
-    try {
-        ConnectCitiesAccordingToGraphWithRoadsOfType(cities, red_graph, 'R');
-        
-        ConnectCitiesAccordingToGraphWithRoadsOfType(cities, blue_graph, 'B');
-    
-        std::cout << "YES\n";
-        
-    } catch (std::logic_error error) {
-        std::cout << "NO\n";
-    }
     
     return 0;
 }
