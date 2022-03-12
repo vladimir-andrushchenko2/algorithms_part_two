a5c27dc4024cabb95c438b6c81490b3b13810640
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-12 20:55:48 +0300

    OK

--- Sprint6TaskB/main.cpp
+++ Sprint6TaskB/main.cpp
@@ -1,138 +1,108 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <unordered_map>
 #include <unordered_set>
-#include <set>
-#include <sstream>
-#include <deque>
+#include <stack>
 #include <cassert>
-#include <stdexcept>
-#include <deque>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <queue>
 
 using VertexId = int;
 
-static constexpr VertexId kFirstId = 1;
+using AdjacencyList = std::vector<std::vector<VertexId>>;
 
-using AdjacencyList = std::vector<std::set<VertexId>>;
+using Matrix = std::vector<std::string>;
 
-AdjacencyList MakeAdjacencyList(int vertex_count) {
-    return AdjacencyList(kFirstId + vertex_count);
-}
-
-struct City {
-    std::unordered_map<VertexId, char> from;
+enum class Color {
+    White, Gray, Black
 };
 
-class Graph {
+struct Graph {
 public:
     Graph() = delete;
-
+    
     Graph(AdjacencyList adjacency_list) :
     adjacency_list_(std::move(adjacency_list)) {}
-
-    void ConnectCities(std::vector<City>& cities, char type) const {
-        auto is_visited = GetVisitedStatus();
-
-        std::deque<VertexId> previous;
-
-        for (VertexId starting_city_id = kFirstId; starting_city_id < cities.size(); ++starting_city_id) {
-            if (!is_visited[starting_city_id]) {
-                DFS(starting_city_id, is_visited, previous, [&cities, type](VertexId current_city, const std::deque<VertexId>& previous){
-                    for (VertexId previously_visited : previous) {
-                        if (cities[current_city].from.count(previously_visited) > 0 && cities[current_city].from[previously_visited] != type) {
-                            throw std::logic_error("duplicate connection of different road types");
+    
+    bool ContainsLoop() const {
+        auto colors = GetArrayOfColor(Color::White);
+        
+        bool found_loop = false;
+        
+        for (VertexId starting_vertex = 0; starting_vertex < colors.size(); ++starting_vertex) {
+            if (colors[starting_vertex] == Color::White) {
+                DFS(starting_vertex, colors, [&](VertexId current_vertex_id) {
+                    for (VertexId adjacent_vertex_id : adjacency_list_[current_vertex_id]) {
+                        if (colors[adjacent_vertex_id] == Color::Gray) {
+                            found_loop = true;
                         }
-
-                        cities[current_city].from[previously_visited] = type;
                     }
                 });
             }
         }
+        
+        return found_loop;
     }
-
-    int AdjacencyListSize() const {
-        return static_cast<int>(adjacency_list_.size());
-    }
-
+    
 private:
-    std::vector<bool> GetVisitedStatus() const {
-        return std::vector(adjacency_list_.size(), false);
+    std::vector<Color> GetArrayOfColor(Color color) const {
+        return {adjacency_list_.size(), color};
     }
-
+    
     template<typename Predicate>
-    void DFS(VertexId start_id, std::vector<bool>& is_visited, std::deque<VertexId>& previous, Predicate predicate) const {
-        predicate(start_id, previous);
-
-        previous.push_back(start_id);
-
-        is_visited[start_id] = true;
-
+    void DFS(VertexId start_id, std::vector<Color>& color_of, Predicate predicate) const {
+        predicate(start_id);
+        
+        color_of[start_id] = Color::Gray;
+        
         for (VertexId adjacent_vertex : adjacency_list_[start_id]) {
-            if (!is_visited[adjacent_vertex]) {
-                DFS(adjacent_vertex, is_visited, previous, predicate);
+            if (color_of[adjacent_vertex] == Color::White) {
+                DFS(adjacent_vertex, color_of, predicate);
             }
         }
-
-        previous.pop_back();
+        
+        color_of[start_id] = Color::Black;
     }
-
+    
 private:
     AdjacencyList adjacency_list_;
 };
 
-std::pair<Graph, Graph> ReadDirectedGraphs(std::istream& input) {
+Graph ReadGraph(std::istream& input) {
     int vertexes_count;
-
+    
     input >> vertexes_count >> std::ws;
-
-    auto adjacency_list_red = MakeAdjacencyList(vertexes_count);
-
-    auto adjacency_list_blue = MakeAdjacencyList(vertexes_count);
-
-    std::string connections_to_other_cities;
-
-    for (VertexId from_vertex_id = 1; from_vertex_id < vertexes_count; ++from_vertex_id) {
-        std::getline(input, connections_to_other_cities);
-
-        for (int i = 0; i < connections_to_other_cities.size(); ++i) {
-            char road_type = connections_to_other_cities[i];
-
-            // + 1 because first cities first road leads to second city
-            VertexId to_vertex_id = from_vertex_id + i + 1;
-
-            if (road_type == 'R') {
-                adjacency_list_red[from_vertex_id].insert(to_vertex_id);
-            } else if (road_type == 'B') {
-                adjacency_list_blue[from_vertex_id].insert(to_vertex_id);
+    
+    AdjacencyList adjacency_list(vertexes_count);
+    
+    std::string row;
+    
+    const int rows_to_read = vertexes_count - 1;
+    
+    for (VertexId from_vertex_id = 0; from_vertex_id < rows_to_read; ++from_vertex_id) {
+        std::getline(input, row);
+        
+        for (int j = 0; j < row.size(); ++j) {
+            VertexId to_vertex_id = from_vertex_id + j + 1;
+            
+            if (row[j] == 'B') {
+                adjacency_list[to_vertex_id].push_back(from_vertex_id);
             } else {
-                assert(false && "Roadtype must be R for Red or B for Blue");
+                adjacency_list[from_vertex_id].push_back(to_vertex_id);
             }
         }
     }
-
-    return {adjacency_list_red, adjacency_list_blue};
+    
+    return Graph{std::move(adjacency_list)};
 }
 
-
-// TODO: try first reading and connecting one by one
-
-
-int main(int argc, const char * argv[]) {
-    auto [red_graph, blue_graph] = ReadDirectedGraphs(std::cin);
-
-    std::vector<City> cities(red_graph.AdjacencyListSize());
-
-    try {
-        red_graph.ConnectCities(cities, 'R');
-
-        blue_graph.ConnectCities(cities, 'B');
-
-        std::cout << "YES\n";
-
-    } catch (std::logic_error error) {
+int main() {
+    if (ReadGraph(std::cin).ContainsLoop()) {
         std::cout << "NO\n";
+    } else {
+        std::cout << "YES\n";
     }
-
+    
     return 0;
 }
